e5389fcc1dc32160569360b9eabcf682f9901af7
Ricardo Luhm <ricardoluhms@gmail.com>
2019-09-18 14:03:28 -0300

    Added new file exp_data which has the Exp_data class - Exp_data requires pandas - Exp_data transform the xlsx file into a dataframe to get experiment data from the excel file

---
+++ cluster/exp_data.py
@@ -0,0 +1,32 @@
+import pandas as pd 
+
+def main():
+    file_path="C:/Users/ricar/Downloads/Experimento_TOF_Texas.xlsx"
+    exp_dt=Exp_data(file_path)
+    from IPython import embed; embed()
+
+class Exp_data():
+    def __init__(self,file_path):
+        self.file_path=file_path
+        self.df=pd.read_excel(self.file_path, header=0,index_col=0)
+        print("Available columns")
+        print(self.df.columns)
+    
+    def get_exp_data(self,exp_number,exp_column_name):
+        if exp_number <=0:
+            print("There is no exp_number with negative values or zero")
+            print(self.df.head())
+        else:
+            data=self.df.loc[[exp_number],[exp_column_name]]
+            value=data.values[0][0]
+            return value
+
+
+
+
+        
+if __name__ == "__main__":
+    main()
+    pass
+
+
