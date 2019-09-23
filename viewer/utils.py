0b4abff9a2aae71921ce4b307af094afcc013b7d
vinicius <vini_ramalho_1051996@hotmail.com>
2019-09-23 11:31:44 -0300

    fix imshow bug for 1 channel image

--- viewer/utils.py
+++ viewer/utils.py
@@ -10,7 +10,7 @@ class pcv():#point cloud viewer
 	def	norm(frame, max=255, dtype='uint8'):
 		frame = frame - frame.min()
 		if frame.max() != 0:
-			frame = (max*frame/frame.max())
+			frame = max*(frame/frame.max())
 		return frame.astype(dtype)
 
 	@staticmethod
@@ -19,14 +19,17 @@ class pcv():#point cloud viewer
 		for channel in range(point_cloud.shape[1]):
 			frame = point_cloud[:,channel].reshape((height, width))
 			frame = pcv.norm(frame)
+			cv2.imshow(window_name, frame)
 			frames.append(frame)
 		frames = np.hstack(frames)
 		cv2.imshow(window_name, frames)
 
 	@staticmethod
-	def pcshow(point_cloud):
+	def pcshow(point_cloud, hide_zero_plane=True):
 		if point_cloud.shape[1] >= 3:
 			point_cloud = point_cloud[:,:3]
+			if hide_zero_plane:
+				point_cloud = point_cloud[point_cloud[:,2]>0,:]
 			point_cloud[:,1] = -point_cloud[:,1]
 			point_cloud[:,2] = -point_cloud[:,2]
 			pcv.pcd.points = o3d.utility.Vector3dVector(point_cloud)
