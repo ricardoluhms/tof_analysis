Fixed distance calculation and depth/point cloud formula

raw_data_processing/utils.py
@@ -20,10 +20,17 @@ class processor():
 		sensor_y = (sensor_y * self.pixel_size) + self.pixel_size / 2 - (self.pixel_size * self.height) / 2
 		self.x_alpha = np.arctan(sensor_x/self.focal_length).reshape((-1,1))
 		self.y_alpha = np.arctan(sensor_y/self.focal_length).reshape((-1,1))
+		self.beta = np.arctan(np.sqrt(np.square(sensor_x)+np.square(sensor_y))/self.focal_length).reshape((-1,1))
 
 	def set_parameters(self, f1, f2):
 		self.freq = np.gcd(int(f1),int(f2))
 		self.R = self.c/(2*self.freq)
+
+	def get_distances(self, phase):
+		phase[phase>2**12-1] = 2**12-1
+		phase = phase.reshape((-1,1))
+		distance = phase*self.R/(2**12)
+		return distance
 
 	def process(self):
 		pass
@@ -34,9 +41,8 @@ class phase2depth(processor):
 		processor.__init__(self, height, width,f1=f1,f2=f2,focal_length=focal_length,pixel_size=pixel_size)
 
 	def process(self, phase):
-		phase = phase.reshape((-1,1))
-		distance = phase*self.R/(2*np.pi)/1000
-		depth = (np.cos(self.y_alpha)*distance)*np.cos(self.x_alpha)
+		distance = self.get_distances(phase)
+		depth = distance*np.cos(self.beta)
 		return depth
 
 
@@ -45,10 +51,9 @@ class phase2point_cloud(processor):
 		processor.__init__(self, height, width,f1=f1,f2=f2,focal_length=focal_length,pixel_size=pixel_size)
 
 	def process(self, phase):
-		phase = phase.reshape((-1,1))
-		distance = phase*self.R/(2*np.pi)/1000
-		x = distance*np.sin(self.x_alpha)
-		y = distance*np.sin(self.y_alpha)
-		z = (np.cos(self.y_alpha)*distance)*np.cos(self.x_alpha)
+		distance = self.get_distances(phase)
+		z = distance*np.cos(self.beta)
+		x = z*np.tan(self.x_alpha)
+		y = z*np.tan(self.y_alpha)
 		point_cloud = np.hstack([x,y,z])
 		return point_cloud